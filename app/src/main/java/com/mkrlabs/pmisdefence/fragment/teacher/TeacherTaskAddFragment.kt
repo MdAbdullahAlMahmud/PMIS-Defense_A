28623b224bb7500c1478110291315c470cd526e6
MdAbdullahAlMahmud <mmunna864@gmail.com>
2023-04-13 01:24:08 +0600

    Add layout ui design

---
+++ app/src/main/java/com/mkrlabs/pmisdefence/fragment/teacher/TeacherTaskAddFragment.kt
@@ -0,0 +1,35 @@
+package com.mkrlabs.pmisdefence.fragment.teacher
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mkrlabs.pmisdefence.R
+import com.mkrlabs.pmisdefence.databinding.FragmentTeacherTaskAddBinding
+
+
+class TeacherTaskAddFragment : Fragment() {
+
+
+    lateinit var binding: FragmentTeacherTaskAddBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentTeacherTaskAddBinding.inflate(layoutInflater)
+
+        return  binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+
+}
