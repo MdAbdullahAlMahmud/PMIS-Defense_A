3fa79bc48f734d7ebb1a4e941b5aed46c6a0dc60
MdAbdullahAlMahmud <mmunna864@gmail.com>
2023-04-15 00:38:36 +0600

    All up to dated before leave dhaka

--- app/src/main/java/com/mkrlabs/pmisdefence/adapter/MyPagerAdapter.kt
+++ app/src/main/java/com/mkrlabs/pmisdefence/adapter/MyPagerAdapter.kt
@@ -7,12 +7,9 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class MyPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
-
     private val fragmentList = mutableListOf<Fragment>()
     private val titleList = mutableListOf<String>()
-
     override fun getItemCount(): Int = fragmentList.size
-
     override fun createFragment(position: Int): Fragment = fragmentList[position]
 
     fun addFragment(fragment: Fragment, title: String) {
